ec07b65a09fbd67f7be217ee2015158ce8349f70
ivansstyle <ivansstyle@me.com>
2016-04-22 22:57:20 +0100

    added vec4.h and vec4.cpp

---
+++ smoke/src/Vec4.cpp
@@ -0,0 +1,101 @@
+#include "Vec4.h"
+#include "Mat4.h"
+
+void Vec4::colourGL() const
+{
+  #warning "implement this"
+}
+
+Vec4 Vec4::cross(const Vec4 &_rhs) const
+{
+
+}
+
+float Vec4::dot(const Vec4 &_rhs) const
+{
+
+}
+
+float Vec4::length() const
+{
+
+}
+
+float Vec4::lengthSquared() const
+{
+
+}
+
+void Vec4::normalGL()
+{
+
+}
+
+void Vec4::normalize()
+{
+
+}
+
+Vec4 Vec4::operator *(const Mat4 &_r) const
+{
+
+}
+
+Vec4 Vec4::operator *(GLfloat _rhs) const
+{
+ return Vec4(m_x*_rhs,m_y*_rhs,m_z*_rhs);
+}
+
+void Vec4::operator *=(GLfloat _rhs)
+{
+    m_x *= _rhs;
+    m_y *= _rhs;
+    m_z *= _rhs;
+}
+
+Vec4 Vec4::operator +(const Vec4 &_r) const
+{
+
+}
+
+void Vec4::operator +=(const Vec4 &_r)
+{
+    m_x += _r.m_x;
+    m_y += _r.m_y;
+    m_z += _r.m_z;
+    m_w += _r.m_w;
+}
+
+Vec4 Vec4::operator -(const Vec4 &_rhs) const
+{
+  return Vec4(m_x-_rhs.m_x,
+              m_y-_rhs.m_y,
+              m_z-_rhs.m_z,
+              m_w);
+}
+
+void Vec4::operator -=(const Vec4 &_r) const
+{
+
+}
+
+bool Vec4::operator ==(const Vec4 &_rhs) const
+{
+
+}
+
+GLfloat & Vec4::operator [](int _i)
+{
+
+}
+
+void Vec4::set(GLfloat _x, GLfloat _y, GLfloat _z, GLfloat _w)
+{
+
+}
+
+void Vec4::vertexGL() const
+{
+
+}
+
---
+++ smoke/src/Vec4.h
@@ -0,0 +1,67 @@
+#ifndef VEC4_H__
+#define VEC4_H__
+#ifdef __linux__
+  #include <GL/gl.h>
+#endif
+#ifdef __APPLE__
+  #include <OpenGL/gl.h>
+#endif
+class Mat4;
+
+class Vec4
+{
+  public:
+    Vec4(const Vec4 &_rhs)=default;
+    Vec4(GLfloat _x=0.0f,
+         GLfloat _y=0.0f,
+         GLfloat _z=0.0f,
+         GLfloat _w=1.0f) :
+      m_x(_x),
+      m_y(_y),
+      m_z(_z),
+      m_w(_w){}
+
+    void colourGL() const; //not modify class as const
+
+    // @ calc cross product of
+    // self and _rhs (self x _rhs)
+    Vec4 cross(const Vec4 &_rhs) const;
+    float dot(const Vec4 &_rhs) const;
+    float length() const;
+    float lengthSquared() const;
+    void normalGL();
+    void normalize();
+
+
+    Vec4 operator *(const Mat4 &_r) const;
+    Vec4 operator *(GLfloat _rhs) const;
+    void operator *=(GLfloat _rhs);
+    Vec4 operator +(const Vec4 &_r) const;
+    void operator +=(const Vec4 &_r);
+    Vec4 operator -(const Vec4 &_rhs) const;
+    void operator -=(const Vec4 &_r) const;
+    bool operator ==(const Vec4 &_rhs) const;
+    GLfloat & operator [](int _i);
+    void set(GLfloat _x, GLfloat _y, GLfloat _z, GLfloat _w=1.0f);
+    void vertexGL() const;
+
+
+  union
+  {
+    GLfloat m_openGL[4];
+    struct
+    {
+      GLfloat m_x;
+      GLfloat m_y;
+      GLfloat m_z;
+      GLfloat m_w;
+    };
+  };
+  private:
+};
+
+
+
+
+
+#endif
